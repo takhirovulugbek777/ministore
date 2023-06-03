57be5bd9f1d4382a68f3f7f279c60aeb6798cb0d
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-06-03 14:57:13 +0500

    change image null= True to default = False item.model

--- item/models.py
+++ item/models.py
@@ -20,7 +20,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     descriptions = models.TextField(blank=True, null=True)
     price = models.FloatField()
-    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    image = models.ImageField(upload_to='item_images', default=False)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
--- shopnew/settings.py
+++ shopnew/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'dashboard',
+    'conversation',
     'item',
 ]
 
@@ -102,7 +103,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+import os
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
