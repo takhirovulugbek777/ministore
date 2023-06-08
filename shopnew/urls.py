a95cb50bdf353df3656d42402b15d7ff07b4dac2
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-06-08 15:13:05 +0500

    ADDED COMMUNICATE SYSTEM

--- shopnew/urls.py
+++ shopnew/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('item/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
